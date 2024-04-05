0eeada996ce49b78805d4b8d7747eaebce6e193f
Muhammed Noushad <mammumuhammed606@gmail.com>
2024-04-06 03:23:29 +0530

    Implemented search functionality

--- frontend/src/components/sidebar/SearchInput.jsx
+++ frontend/src/components/sidebar/SearchInput.jsx
@@ -1,17 +1,45 @@
+import { useState } from "react";
 import { IoSearchSharp } from "react-icons/io5";
+import useConversation from "../../zustand/useConversation";
+import useGetConversations from "../../hooks/useGetConversations";
+import toast from "react-hot-toast";
 
 function SearchInput() {
+  const [search, setSearch] = useState();
+  const { setSelectedConversation } = useConversation();
+  const { conversations } = useGetConversations();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!search) return;
+    if (search.length < 3)
+      toast.error("Search term must be atleast 3 character long ");
+    const conversation = conversations.find((c) =>
+      c.fullName.toLowerCase().includes(search.toLowerCase())
+    );
+
+    if (conversation) {
+      setSelectedConversation(conversation);
+      setSearch("");
+    } else {
+      toast.error("No user found");
+    }
+  };
+
   return (
-    <div className="flex items-center gap-2">
+    <form className="flex items-center gap-2" onSubmit={handleSubmit}>
       <input
         type="text"
         placeholder="Search..."
         className="input input-bordered rounded-full"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
       />
       <button className="btn btn-circle bg-blue-500 text-white">
         <IoSearchSharp className="w-6 h-6 outline-none" />
       </button>
-    </div>
+    </form>
   );
 }
 
