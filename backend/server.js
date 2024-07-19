a99fa3ef035678aefc00392d33064ba49eb3460e
Muhammed Noushad <mammumuhammed606@gmail.com>
2024-07-19 10:06:20 +0530

    Added mongodb connection function inside the server file

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,6 @@ import { app, server } from "./socket/socket.js";
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoute from "./routes/user.routes.js";
-import connectToMongoDB from "./db/connectToMongodb.js";
 
 import portectRoutes from "./middleware/protectRoutes.js";
 
@@ -28,6 +27,15 @@ app.use(express.static(path.join(__dirname, "frontend/dist")));
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
+
+const connectToMongoDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_DB_URI);
+    console.log("connected to mongoDB");
+  } catch (error) {
+    console.log("Error while connecting to mongoDB", error.message);
+  }
+};
 
 server.listen(PORT, () => {
   connectToMongoDB();
