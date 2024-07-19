c9f4549227934c7b44dcc23f345796e7cdf0ab6d
Muhammed Noushad <mammumuhammed606@gmail.com>
2024-07-19 09:46:48 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 import { app, server } from "./socket/socket.js";
 import authRoutes from "./routes/auth.routes.js";
@@ -12,6 +13,8 @@ import portectRoutes from "./middleware/protectRoutes.js";
 
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 dotenv.config();
 
 app.use(express.json());
@@ -20,6 +23,11 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/message", portectRoutes, messageRoutes);
 app.use("/api/users", portectRoutes, userRoute);
+
+app.use(express.static(path.join(__dirname, "frontend/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(PORT, () => {
   connectToMongoDB();
